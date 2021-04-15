3520234ab4676e0d761fea0013427abbc285a758
limeng <limeng365@163.com>
2021-04-15 18:04:13 +0800

    first commit

---
+++ src/main/java/com/test/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.test.controller;
+
+/**
+ * @Author: limeng
+ * @Date: 2021/4/15 18:03
+ * @Description:
+ * @Version: 1.0
+ */
+
+public class UserController {
+
+	private String userName;
+}
